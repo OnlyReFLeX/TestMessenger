f17a1540386f091b61e0944e6ea1e5364a330596
OnlyReFLeX <reflexonly@gmail.com>
2017-11-21 22:45:16 +0300

    add devise & locales & model user &

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_last_seen_at, if: proc { user_signed_in? && (session[:last_seen_at] == nil || session[:last_seen_at] < 1.minutes.ago) }
+
+  protected
+
+  def set_last_seen_at
+    current_user.update_column(:last_seen_at, Time.current)
+    session[:last_seen_at] = Time.current
+  end
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def last_seen_at_or_online(last_seen_at)
+    last_seen_at = last_seen_at.in_time_zone('Moscow')
+    if last_seen_at < 15.minutes.ago
+      "Был в сети: #{last_seen_at.try(:strftime, "%d.%m.%Y %H:%M")}"
+    else
+      "Online"
+    end
+  end
 end
